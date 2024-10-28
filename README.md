bccc4f9e5b466b19adfe103019707793fb419580
briannaordaz <briannaordaz5@gmail.com>
2024-10-27 20:16:53 -0700

    finished script1.js and script2.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # COMP002Assignment2Week2
+Author: Brianna Ordaz
+Purpose: to save any javascript coding 
---
+++ assignment03/script1.js
@@ -0,0 +1,17 @@
+// 1. Write code to display a triangle in the console that looks similar to the following.
+// You should use a loop to accomplish this.
+// #
+// ##
+// ###
+// ####
+// ###
+// ##
+// #
+
+for (let i = 1; i <= 4; i++) {
+    console.log('?'.repeat(i));
+}
+
+for (let i = 3; i >= 1; i--) {
+    console.log('?'.repeat(i));
+}
